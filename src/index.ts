7bfbf3c26f6091969afa7eddaaa9f23f2ae08597
Roger Chappel <miscanalysis@gmail.com>
2026-05-18 18:47:30 +1000

    feat: export config initializer

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { scanProject } from "./core/audit.js";
 export { classifyCommand } from "./core/classify.js";
 export { discoverCommands } from "./discover/index.js";
 export { renderReport } from "./render/index.js";
+export { initConfig } from "./init.js";
 export type {
   AuditConfig,
   AuditReport,
